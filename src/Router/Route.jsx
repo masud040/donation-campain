3300aa84f78aaab1ad2e5459c0d08161b55bdc62
masudo40 <masudinfo128@gamil.com>
2023-09-24 19:43:51 +0600

    test my repository for this assignment

---
+++ src/Router/Route.jsx
@@ -0,0 +1,10 @@
+import { createBrowserRouter } from "react-router-dom";
+
+const MyCreatedRoute = createBrowserRouter([
+  {
+    path: "/",
+    element: <div>Hello World</div>,
+  },
+]);
+
+export default MyCreatedRoute;
---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,9 @@
+const Home = () => {
+  return (
+    <div>
+      <div className="bg-"></div>
+    </div>
+  );
+};
+
+export default Home;
